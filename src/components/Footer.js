7b9c8fe917f298cd97a924086a516ee9436cc1cf
Cesar Chávez <design@pinata.tech>
2019-05-14 17:05:30 -0500

    init

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export const Footer = () => {
           </section>
           <section className="__col_xs_12 __col_sm_6 __col_md_4">
             <h2 className={s.__h2}>Contact</h2>
-            <a rel="noopener noreferrer" className={s.__social} href="mailto:steffo@cannacter.com?Subject=Hello!"><Mail size={24}/>hello@cannacter.com</a>
+            <a rel="noopener noreferrer" className={s.__social} href="mailto:hello@cannacter.com?Subject=Hello!"><Mail size={24}/>hello@cannacter.com</a>
             <a rel="noopener noreferrer" className={s.__social} target="_blank" href="https://www.facebook.com/Cannacter-1839526109472881"><Facebook size={24}/>fb/cannacter</a>
             <a rel="noopener noreferrer" className={s.__social} target="_blank" href="https://www.facebook.com/events/2904419576257345/"><Calendar size={24}/>Facebook Events</a>
             <a rel="noopener noreferrer" className={s.__social} target="_blank" href="https://www.instagram.com/cannacter/?hl=en"><Instagram size={24}/>@cannacter</a>
